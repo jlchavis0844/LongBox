e07c20588527c302da4a7e5fe10be8269423a2ac
James <James@JamesDesktop>
2016-09-26 23:58:12 -0700

    Added popup windows to test picture getting code. getIssue now works.

--- src/CVrequest.java
+++ src/CVrequest.java
@@ -1,4 +1,6 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.XML;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
@@ -26,7 +28,25 @@ public class CVrequest {
 	/**
 	 * Default search, limit is 10, no given resource
 	 * @param qString = the term to search for 
-	 * @return JsonNode of the response body
+	 * @return JsonNode of the response body, values are:<br>
+	 * image JSON array
+	 * Aliases
+	 * Gender
+	 * "site_detail_url"
+	 * "deck"
+	 * "origin" array
+	 * "resource_type"
+	 * "birth"
+	 * "description"
+	 * "real_name"
+	 * "api_detail_url"
+	 * "date_added"
+	 * "name"
+	 * "publisher" array
+	 * "id"
+	 * "first_appeared_in_issue"
+	 * "count_of_issue_appearances"
+	 * "date_last_updated"
 	 */
 	public JsonNode search(String qString){
 		try {
@@ -50,7 +70,7 @@ public class CVrequest {
 	 * @param qString - String to search for
 	 * @param rString - String of the resource being searched
 	 * @param limit - int of how many results to return, max of 100
-	 * @param fieldList - Array of Strings of field names to return:
+	 * @param fieldList - Array of Strings of field names to return in addition to header:
 	 * 	character
 	 * 	concept
 	 * 	origin
@@ -63,7 +83,7 @@ public class CVrequest {
 	 * 	person
 	 * 	team
 	 * 	video
-	 * @return JsonNode of the response body.
+	 * @return JsonNode of the response body, depends on resource
 	 */
 	public JsonNode search(String qString, String rString, int lim, String[] fieldList){
 		String fList = "";
@@ -99,7 +119,7 @@ public class CVrequest {
 	 * @param qString - String to search for
 	 * @param limit - int of how many results to return, max of 100
 	 * @param rString - what resource to search for
-	 * @return JsonNode of the response body
+	 * @return JsonNode of the response body, depends on resource
 	 */
 	public JsonNode search(String qString, int limit, String rString){
 		try {
@@ -119,7 +139,14 @@ public class CVrequest {
 		return response;
 	}
 
-	public JsonNode searchVolume(String qString){
+	/**
+	 * Easy volume search, returns <b>WITHOUT</b> header.
+	 * @param qString - what volume to search for
+	 * @return JSONArray of the search results, <b>WITHOUT</b> header, fields:
+	 * name<br>start_year<br>publisher array<br>id<br>count_of_issues<br>image array
+	 * 
+	 */
+	public JSONArray searchVolume(String qString){
 		try {
 			response = Unirest.get(baseURL + "/search")
 					.header("Accept", "application/json")
@@ -127,19 +154,19 @@ public class CVrequest {
 					.queryString("client","cvscrapper")
 					.queryString("query", qString)
 					.queryString("resources", "volume")
-					.queryString("field_list", "name,start_year,publisher,id,count_of_issues")
+					.queryString("field_list", "name,start_year,publisher,id,count_of_issues,image")
 					.queryString("format", "json")
 					.queryString("limit", "100")
 					.asJson().getBody();
-			return response;
+			return response.getObject().getJSONArray("results");
 		} catch (UnirestException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return response;
+		return null;
 	}
 
-	public JsonNode getVolumeYears(String volID){
+	public JsonNode getVolumeInfo(String volID){
 		try {
 			String query = "http://comicvine.gamespot.com/api/volume/4050-" + volID;
 			response = Unirest.get(query)
@@ -176,5 +203,21 @@ public class CVrequest {
 			e.printStackTrace();
 		}
 		return response;
+	}
+	
+	public JSONObject getIssue(String issId){
+		try {
+			String query = "http://comicvine.gamespot.com/api/issue/4000-" + issId;
+			HttpResponse<String> temp = Unirest.get(query)
+					.queryString("api_key", api_key)
+					.asString();
+			JSONObject jo = XML.toJSONObject(temp.getBody());
+			jo = jo.getJSONObject("response");
+			return (JSONObject) jo.get("results");
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 };
